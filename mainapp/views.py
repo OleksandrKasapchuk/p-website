e00432255bd6c978ce23de365253a8b04464db26
sashapre228 <sasha291108@gmail.com>
2024-11-16 21:33:02 +0100

    initial feature

---
+++ mainapp/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from .models import *
+from django.views.generic import View, ListView, DetailView, CreateView, TemplateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+
+
+class IndexView(TemplateView):
+	template_name = 'mainapp/index.html'
